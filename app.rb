cc0effdf83b126150462a08906d8e3dd237c2a94
github username <github email address>
2019-06-04 11:49:58 -0700

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
     erb :food_form
   end
 
+  post '/food' do
+    return "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+
   # Add your post route and action below
 
 end
